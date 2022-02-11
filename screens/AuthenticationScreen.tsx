79bf080bd9785453aabe242d93e3d828db2f549e
mveres <veres85@yahoo.com>
2022-02-11 12:04:55 +0200

    fixes

--- screens/AuthenticationScreen.tsx
+++ screens/AuthenticationScreen.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Button, Caption } from 'react-native-paper';
+import { Button, Title } from 'react-native-paper';
 import { RootStackScreenProps } from '../types';
 
 const styles = StyleSheet.create({
   container: {
+    margin: 20,
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    height: 64,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    width: '100%',
+    marginBottom: 26,
   },
 });
 
@@ -17,14 +27,15 @@ export const AuthenticationScreen = ({
 }: RootStackScreenProps<'Authentication'>) => {
   return (
     <View style={styles.container}>
+      <Title style={styles.title}>Intră în cont</Title>
       <Button
+        style={styles.button}
         icon="map"
         mode="contained"
         onPress={() => navigation.navigate('Main')}
       >
-        Login
+        Intră în cont
       </Button>
-      <Caption>Open up App.tsx to start working on your app!</Caption>
     </View>
   );
 };
