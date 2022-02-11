efe5c3afd4f18133d8c9cd48e61426d09ecec3e3
sebi-dan-csi <sebi@craftingsoftware.com>
2022-02-11 12:18:45 +0200

    Added initial region.

---
+++ models/Hotspot.ts
@@ -0,0 +1,11 @@
+export type Hotspot = {
+  latitude: number;
+  longitude: number;
+};
+
+export const mockData: Hotspot[] = [
+  {
+    latitude: 46.77119840460617,
+    longitude: 23.59210914443709,
+  },
+];
--- screens/MainScreen.tsx
+++ screens/MainScreen.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Dimensions, StyleSheet, View } from 'react-native';
-import MapView from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
+import MapView, { Region } from 'react-native-maps';
 
 export const MainScreen = () => {
   return (
     <View style={styles.container}>
       <MapView
+        initialRegion={getInitialRegion()}
         style={styles.map}
         onLongPress={e => alert(JSON.stringify(e.nativeEvent.position.x))}
       />
@@ -13,15 +14,24 @@ export const MainScreen = () => {
   );
 };
 
+const getInitialRegion = (): Region => {
+  return {
+    latitude: 46.77293258116839,
+    longitude: 23.587688864363546,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  };
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    // justifyContent: 'center',
+    justifyContent: 'center',
   },
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    width: '100%',
+    height: '50%',
   },
 });
