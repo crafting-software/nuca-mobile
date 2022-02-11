19f8a6627462376c7df3aaaf70fbbb5cabd33bbb
Debre Rebeka <rebeka@craftingsoftware.com>
2022-02-11 16:26:42 +0200

    remove auth changes

--- screens/AuthenticationScreen.tsx
+++ screens/AuthenticationScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { CatCard } from '../components/CatCard';
+import { Button, Title } from 'react-native-paper';
 import { RootStackScreenProps } from '../types';
 
 const styles = StyleSheet.create({
@@ -27,7 +27,7 @@ export const AuthenticationScreen = ({
 }: RootStackScreenProps<'Authentication'>) => {
   return (
     <View style={styles.container}>
-      {/* <Title style={styles.title}>Intră în cont</Title>
+      <Title style={styles.title}>Intră în cont</Title>
       <Button
         style={styles.button}
         icon="map"
@@ -35,22 +35,7 @@ export const AuthenticationScreen = ({
         onPress={() => navigation.navigate('Main')}
       >
         Intră în cont
-      </Button> */}
-      <CatCard
-        cat={{
-          sex: 'F',
-          notes: 'Nothing to see here',
-          checkInDate: new Date(),
-          checkOutDate: new Date(),
-          isSterilized: true,
-          capturedBy: { name: 'Ionel' },
-          media: [
-            'https://picsum.photos/200/300?random=1',
-            'https://picsum.photos/200/300?random=2',
-          ],
-        }}
-        index={1}
-      ></CatCard>
+      </Button>
     </View>
   );
 };
