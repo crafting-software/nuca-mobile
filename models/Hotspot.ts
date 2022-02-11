911b7ba77da189e8be892781b41cce30bd85ae34
sebi-dan-csi <sebi@craftingsoftware.com>
2022-02-11 12:26:19 +0200

    Show map markers from dummy data.

--- models/Hotspot.ts
+++ models/Hotspot.ts
@@ -1,11 +1,21 @@
 export type Hotspot = {
+  name: string;
+  description: string;
   latitude: number;
   longitude: number;
 };
 
 export const mockData: Hotspot[] = [
   {
+    name: 'City center',
+    description: 'near city center',
     latitude: 46.77119840460617,
     longitude: 23.59210914443709,
+  },
+  {
+    name: 'EC Garden',
+    description: 'near EC Garden',
+    latitude: 46.77560721858744,
+    longitude: 23.577346267298275,
   },
 ];
--- screens/MainScreen.tsx
+++ screens/MainScreen.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import MapView, { Region } from 'react-native-maps';
+import MapView, { Marker, Region } from 'react-native-maps';
+import { Hotspot, mockData as HotspotMockData } from '../models/Hotspot';
+
+export const hotspots: Hotspot[] = HotspotMockData;
 
 export const MainScreen = () => {
   return (
@@ -9,7 +12,19 @@ export const MainScreen = () => {
         initialRegion={getInitialRegion()}
         style={styles.map}
         onLongPress={e => alert(JSON.stringify(e.nativeEvent.position.x))}
-      />
+      >
+        {hotspots.map((hotspot, index) => (
+          <Marker
+            key={index}
+            coordinate={{
+              latitude: hotspot.latitude,
+              longitude: hotspot.longitude,
+            }}
+            title={hotspot.name}
+            description={hotspot.description}
+          />
+        ))}
+      </MapView>
     </View>
   );
 };
@@ -32,6 +47,6 @@ const styles = StyleSheet.create({
   },
   map: {
     width: '100%',
-    height: '50%',
+    height: '100%',
   },
 });
