24c1ac2bcdd3cb08cbce04f12bff62ec0f2389db
mveres <veres85@yahoo.com>
2022-02-11 11:43:37 +0200

    fonts and colors

--- App.tsx
+++ App.tsx
@@ -1,17 +1,60 @@
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import {
+  Provider as PaperProvider,
+  Button,
+  Caption,
+  Text,
+} from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {
+  useFonts,
+  Nunito_200ExtraLight,
+  Nunito_300Light,
+  Nunito_400Regular,
+  Nunito_500Medium,
+  Nunito_700Bold,
+  Nunito_400Regular_Italic,
+} from '@expo-google-fonts/nunito';
 import Navigation from './navigation/index';
+import { getTheme } from './theme';
 
-export default function App() {
+export default () => {
+  const [fontsLoaded] = useFonts({
+    Nunito_200ExtraLight,
+    Nunito_300Light,
+    Nunito_400Regular,
+    Nunito_500Medium,
+    Nunito_700Bold,
+    Nunito_400Regular_Italic,
+  });
+
+  if (!fontsLoaded) return <AppLoading />;
+
+  const theme = getTheme();
+
   return (
-    <SafeAreaProvider>
-      <Navigation />
-      <StatusBar />
-    </SafeAreaProvider>
+    <PaperProvider theme={theme}>
+      <SafeAreaProvider>
+        <Navigation />
+        <StatusBar />
+        <View style={styles.container}>
+          <Button
+            icon="camera"
+            mode="contained"
+            onPress={() => console.log('Pressed')}
+          >
+            Press me
+          </Button>
+          <Caption>Open up App.tsx to start working on your app!</Caption>
+          <StatusBar style="auto" />
+        </View>
+      </SafeAreaProvider>
+    </PaperProvider>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
