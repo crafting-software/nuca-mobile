c680a997f0897e636d01b6ae5a8cda5f1afe674f
Debre Rebeka <rebeka@craftingsoftware.com>
2022-08-24 14:13:27 +0300

    delete modal

--- screens/ChooseLocationScreen.tsx
+++ screens/ChooseLocationScreen.tsx
@@ -91,19 +91,6 @@ export const ChooseLocationScreen = ({
           }}
           showsUserLocation
           style={styles.map}
-          //am incercat si cu asta, asta merge, dar nu face ceea ce vrem noi
-          onRegionChangeComplete={async region => {
-            console.log('Bent1 ', region);
-            const { latitude, longitude } = region;
-            const isSameMarker =
-              selectedLocation?.longitude === longitude &&
-              selectedLocation?.latitude === latitude;
-
-            if (!isSameMarker) {
-              const location = await findPlace(latitude, longitude);
-              setSelectedLocation(location);
-            }
-          }}
           onPress={async e => {
             const getCoords = () => {
               if (Platform.OS === 'web') {
@@ -171,31 +158,33 @@ export const ChooseLocationScreen = ({
             </Marker>
           ) : null}
         </MapView>
-        <TextInput
-          ref={searchAddressRef}
-          multiline={false}
-          dense={true}
-          scrollEnabled={true}
-          blurOnSubmit={true}
-          outlineColor={theme.colors.disabled}
-          mode="outlined"
-          style={[styles.searchInput, { height: 60 }]}
-          autoCorrect={false}
-          placeholder="Caută"
-          autoComplete={false}
-          right={
-            <TextInput.Icon name="magnify" color={theme.colors.placeholder} />
-          }
-          returnKeyType="search"
-          onSubmitEditing={async ({ nativeEvent: { text } }) => {
-            setSelectedAddress(text);
-            searchForAddress(text);
-          }}
-          onChangeText={text => {
-            setSelectedAddress(text);
-          }}
-          value={selectedAddress}
-        />
+        <View style={styles.searchInput}>
+          <TextInput
+            ref={searchAddressRef}
+            multiline={false}
+            dense={true}
+            scrollEnabled={true}
+            blurOnSubmit={true}
+            outlineColor={theme.colors.disabled}
+            mode="outlined"
+            style={{ height: 60 }}
+            autoCorrect={false}
+            placeholder="Caută"
+            autoComplete={false}
+            right={
+              <TextInput.Icon name="magnify" color={theme.colors.placeholder} />
+            }
+            returnKeyType="search"
+            onSubmitEditing={async ({ nativeEvent: { text } }) => {
+              setSelectedAddress(text);
+              searchForAddress(text);
+            }}
+            onChangeText={text => {
+              setSelectedAddress(text);
+            }}
+            value={selectedAddress}
+          />
+        </View>
       </View>
 
       {selectedLocation ? (
@@ -287,12 +276,12 @@ const getStyles = (theme: Theme, insets: EdgeInsets) =>
       left: 20,
       right: 20,
       elevation: 6,
-      paddingHorizontal: 8,
       height: 60,
-
       shadowColor: theme.colors.text,
-      shadowOffset: { width: 2, height: 4 },
-      shadowOpacity: 0.2,
+      shadowOffset: { width: 0, height: 1 },
+      shadowOpacity: 0.22,
+      shadowRadius: 0.22,
+      borderRadius: 30,
     },
     marker: {
       width: 40,
