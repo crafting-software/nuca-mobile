9875ca366f008092d16f740fa04eb622c2b2df53
Salajan Dan <salajan.dan11@gmail.com>
2024-04-24 10:57:07 +0300

    fixed hotspot cards width

--- utils/helperFunc.ts
+++ utils/helperFunc.ts
@@ -1,3 +1,5 @@
 import { Dimensions } from 'react-native';
 
 export const isSmallScreen = () => Dimensions.get('window').width < 720;
+
+export const isSmallMobileScreen = () => Dimensions.get('window').width <= 400;
