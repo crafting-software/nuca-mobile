ef70106a7736e1498d63f3a80d771feee5b9a81a
Salajan Dan <salajan.dan11@gmail.com>
2024-05-07 13:55:19 +0300

    fixed min width

--- utils/helperFunc.ts
+++ utils/helperFunc.ts
@@ -2,4 +2,4 @@ import { Dimensions } from 'react-native';
 
 export const isSmallScreen = () => Dimensions.get('window').width < 720;
 
-export const isSmallMobileScreen = () => Dimensions.get('window').width <= 400;
+export const isSmallMobileScreen = () => Dimensions.get('window').width <= 430;
